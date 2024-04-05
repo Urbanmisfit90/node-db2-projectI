68c6c781d360131e5f69e1c92a81e93b7d0e8f29
Robert Dixon <robmsc24@gmail.com>
2024-04-04 21:59:02 -0400

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,20 @@
-exports.up = function (knex) {
   // DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', function(tbl) {
+    tbl.increments(); // auto-incrementing id field
+
+    tbl.string('vin', 17).notNullable().unique(); // Assuming VIN is a string of length 17 and should be unique
+
+    tbl.string('make', 128).notNullable();
+
+    tbl.string('model', 128).notNullable();
+
+    tbl.numeric('mileage').unsigned().notNullable();
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  });
 };
 
 exports.down = function (knex) {
